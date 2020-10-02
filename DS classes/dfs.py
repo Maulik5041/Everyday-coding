987cc791bfbc439003ee497e87319dcdd8b428cc
maulik_padh <maulik.padh@monsanto.com>
2020-10-02 12:39:05 -0500

    Update DS classes/dfs.py

---
+++ DS classes/dfs.py
@@ -0,0 +1,16 @@
+class Node:
+    def __init__(self, name):
+        self.children = []
+        self.name = name
+
+    def add_child(self, name):
+        self.children.append(Node(name))
+
+    # Time = O(V + E) Space = O(V)
+    def depth_first_search(self, array):
+        array.append(self.name)
+
+        for child in self.children:
+            child.depth_first_search(array)
+
+        return array
